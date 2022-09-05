c5964dddc5a511671400c2c803bf57f8bf5abe4d
Zhang Jinming <553781523@qq.com>
2022-09-05 08:38:48 +0800

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @author Zhang Jinming
+ * &#064;date  5/9/2022 上午8:19
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
